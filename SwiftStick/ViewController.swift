df7584e09329ceffe02c2df9fb77f6eaef34c306
Joakim Gyllstrom <joakim@backslashed.se>
2014-10-29 22:35:10 +0100

    Initial Commit

---
+++ SwiftStick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStick
+//
+//  Created by Joakim Gyllström on 2014-10-29.
+//  Copyright (c) 2014 Joakim Gyllström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
